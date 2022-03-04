408d609856a35c1dc969bdef833b60cd0eaacc65
mdhos <mdhos@LAPTOP-LPO1AM8H>
2022-03-04 11:53:52 -0500

    First Commit

---
+++ myScala/src/FirstScala.scala
@@ -0,0 +1,9 @@
+
+
+class FirstScala {
+  
+  def main(args:Array[String]){
+    
+  }
+  
+}
---
+++ myScala/src/myObject.scala
@@ -0,0 +1,23 @@
+
+object myObject {
+   def main(args:Array[String]){  
+        println("Hello Scala")
+        
+        
+        var a:Int =10;
+        println(a);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    } 
+}
